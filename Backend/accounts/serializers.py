d02df4184703918687240a20b00246c48bb5a967
akash.chauhan <akash.chauhan@ebix.com>
2026-03-21 18:57:36 +0530

    register api created

--- Backend/accounts/serializers.py
+++ Backend/accounts/serializers.py
@@ -3,7 +3,11 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=4, style={'input_type': 'password'})
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
