f779147c147e5d86fad09d8ddc7b5128142b35ad
akash.chauhan <akash.chauhan@ebix.com>
2026-04-28 12:05:55 +0530

    token expire added and protected view added

--- Backend/accounts/urls.py
+++ Backend/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, ProtectedView
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
+    path('protected-view/', ProtectedView.as_view()),
 ]
--- Backend/accounts/views.py
+++ Backend/accounts/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 from .serializers import UserSerializer
-
 
 class RegisterView(generics.CreateAPIView):
     quaryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
 
+    def get(self, request):
+        response = {
+            'message': 'You are authenticated',
+            'user': request.user.username
+        }
+        return Response(response, status=status.HTTP_200_OK)
